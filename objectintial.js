13ca23d1ae62a72a0c9ae22921e0df1d4f97cecd
Kevin <ketsang12@gmail.com>
2018-06-13 14:59:09 -0400

    objectinital.js

--- objectintial.js
+++ objectintial.js
@@ -1,6 +1,6 @@
+'use strict';
 
-
-// let loaf = {
+// const loaf = {
 //   flour: 300,
 //   water: 210,
 //   hydration: function (){
@@ -9,17 +9,32 @@
 // }
 // console.log(loaf.hydration());
 
-let threemusk ={
-  foo: 'bee',
-  bar: 'splat',
-  fum: ' xlkuoiy',
-  quux: 'bobby',
-  spam: 'luyoiu',
-}
-for(const key in threemusk){
-  console.log(key, threemusk[key]);
+// const threemusk ={
+//   foo: 'bee',
+//   bar: 'splat',
+//   fum: ' xlkuoiy',
+//   quux: 'bobby',
+//   spam: 'luyoiu',
+// };
 
-}
+// for(const key in threemusk){
+//   console.log(key, threemusk[key]);
+// }
+
+const mealTime = {
+  meals: [
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+  ],
+};
+
+console.log(mealTime.meals[4]);
+
 
 
 
