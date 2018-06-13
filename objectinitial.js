8e1f8e50e3cfbd607a7c70b63a8e561ece7edb29
Kevin <ketsang12@gmail.com>
2018-06-13 15:13:34 -0400

    updated objectinitial.js

---
+++ objectinitial.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function (){
+//     return (this.water / this.flour) * 100;
+//   }
+// }
+// console.log(loaf.hydration());
+
+// const threemusk ={
+//   foo: 'bee',
+//   bar: 'splat',
+//   fum: ' xlkuoiy',
+//   quux: 'bobby',
+//   spam: 'luyoiu',
+// };
+
+// for(const key in threemusk){
+//   console.log(key, threemusk[key]);
+// }
+
+// const mealTime = {
+//   meals: [
+//     'breakfast',
+//     'second breakfast',
+//     'elevenses',
+//     'lunch',
+//     'afternoon tea',
+//     'dinner',
+//     'supper'
+//   ],
+// };
+
+// console.log(mealTime.meals[3]);
+
+// let myArray = [
+//   {
+//     name: 'Rick',
+//     jobTitle: 'scientist',
+//   },
+//   {
+//     name: 'Morty',
+//     jobTitle: 'lackey',
+//   },
+//   {
+//     name: 'Homer',
+//     jobTitle: 'slacker',
+//   }
+// ];
+
+// for (let i = 0; i < myArray.length; i++){
+//   for (const key in myArray[i]){
+//     console.log(myArray[i][key]);
+//   }
+// }
+
+
+
