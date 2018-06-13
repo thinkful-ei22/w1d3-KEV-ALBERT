7ae64f83c28bc7b81ed0ced81a25d8269c955450
Albert Fougy <albert@fougy.com>
2018-06-13 14:50:24 -0400

    added key/pair

---
+++ objectintial.js
@@ -0,0 +1,26 @@
+
+
+// let loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function (){
+//     return (this.water / this.flour) * 100;
+//   }
+// }
+// console.log(loaf.hydration());
+
+let threemusk ={
+  foo: 'bee',
+  bar: 'splat',
+  fum: ' xlkuoiy',
+  quux: 'bobby',
+  spam: 'luyoiu',
+}
+for(const key in threemusk){
+  console.log(key, threemusk[key]);
+
+}
+
+
+
+
